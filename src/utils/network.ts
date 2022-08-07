602217256252bc47b5da7035c1c0ee9730c1aa12
Angie Siu <siuangie91@gmail.com>
2022-08-07 04:03:02 -0400

    chore: delete obsolete network utils

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,14 +1,6 @@
 // TODO test
 import axios from 'axios';
 
-const POKEAPI_URL_BASE = 'https://pokeapi.co/api/v2';
-
-const POKEAPI_URLS = {
-  GEN_1: `${POKEAPI_URL_BASE}/pokedex/1`,
-  POKEMON: `${POKEAPI_URL_BASE}/pokemon/`,
-  SPECIES: `${POKEAPI_URL_BASE}/pokemon-species/`,
-};
-
 const POKEAPI_GQL_URL = 'https://beta.pokeapi.co/graphql/v1beta';
 
 export const fetchGraphQL = async <ResponseShape>(
@@ -25,50 +17,6 @@ export const fetchGraphQL = async <ResponseShape>(
       variables,
       operationName,
     });
-
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-/**
- * Fetches the first 20 entries for Gen 1 pokemon from PokeAPI
- * @returns
- */
-export const getFirst20PokemonEntries = async () => {
-  try {
-    const { data } = await axios.get<PokemonAPI.Generation>(POKEAPI_URLS.GEN_1);
-
-    const { pokemon_entries } = data;
-
-    return pokemon_entries.length ? pokemon_entries.slice(0, 20) : [];
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-/**
- * Fetch the Pokemon data by name
- * @param name
- */
-export const getPokemonByName = async (name: string) => {
-  try {
-    const { data } = await axios.get<PokemonAPI.Pokemon>(`${POKEAPI_URLS.POKEMON}${name}`);
-
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-/**
- * Fetch the Pokemon's species data by name
- * @param name
- */
-export const getSpeciesByName = async (name: string) => {
-  try {
-    const { data } = await axios.get<PokemonAPI.Species>(`${POKEAPI_URLS.SPECIES}${name}`);
 
     return data;
   } catch (error) {
