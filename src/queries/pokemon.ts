b70853e918ac448a21dde61d9a95d42ee02ddab6
Angie Siu <siuangie91@gmail.com>
2022-08-07 03:24:32 -0400

    feat: pokemon query

---
+++ src/queries/pokemon.ts
@@ -0,0 +1,44 @@
+import gql from 'gql-tag';
+
+const pokemonQuery = gql`
+  query Pokemon($id: String!) {
+    pokemon: pokemon_v2_pokemon(where: { name: { _eq: $id } }) {
+      id
+      name
+      types: pokemon_v2_pokemontypes {
+        type: pokemon_v2_type {
+          name
+        }
+      }
+      species: pokemon_v2_pokemonspecy {
+        name
+        description: pokemon_v2_pokemonspeciesflavortexts(
+          where: {
+            language_id: { _eq: 9 } # en-us
+          }
+          order_by: { id: desc }
+          limit: 3
+        ) {
+          id
+          flavor_text
+        }
+        evolution_chain: pokemon_v2_evolutionchain {
+          chain: pokemon_v2_pokemonspecies {
+            id
+            name
+          }
+        }
+      }
+      pokemon_type: pokemon_v2_pokemontypes {
+        type: pokemon_v2_type {
+          name
+        }
+      }
+      images: pokemon_v2_pokemonsprites {
+        sprites
+      }
+    }
+  }
+`;
+
+export default pokemonQuery;
