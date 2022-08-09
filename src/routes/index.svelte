15f267ac7225c6c5f06b52762f6c8b88ee5d5ebc
Angie Siu <siuangie91@gmail.com>
2022-08-08 23:11:23 -0400

    refactor: use tailwind styles;

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,35 +24,12 @@
   <title>Svelte Pokédex</title>
 </svelte:head>
 
-<main>
-  <h1>Svelte Pokédex</h1>
-  <Selection entryAddedHandler={handleAdd} />
-  <Seen {entries} />
-</main>
+<h1>Svelte Pokédex</h1>
+<Selection entryAddedHandler={handleAdd} />
+<Seen {entries} />
 
 <style>
-  :global(*) {
-    font-family: sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  :global(html) {
-    background: url(http://pngimg.com/uploads/pokeball/pokeball_PNG8.png) no-repeat;
-    background-position: center center;
-    background-size: contain;
-  }
-
-  :global(body) {
-    background: rgba(255, 255, 255, 0.5);
-  }
-
-  main {
-    padding: 10px;
-    text-align: center;
-  }
-
   h1 {
-    margin: 10px auto;
+    @apply text-center;
   }
 </style>
