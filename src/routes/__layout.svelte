e55237989138a6848cc2e167eaf750b7c5c632f2
Angie Siu <siuangie91@users.noreply.github.com>
2022-08-09 17:03:46 -0400

    feat: handle when all seen and add sticky footer (#40)
    * feat: display message when all are seen
    
    * style: background
    
    * feat: add sticky footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,3 +5,31 @@
 <main>
   <slot />
 </main>
+
+<footer>
+  See on <a href="https://github.com/siuangie91/svelte-pokedex" target="_blank">GitHub</a>
+</footer>
+
+<style lang="postcss">
+  main {
+    @apply px-4;
+    @apply py-4;
+    flex-grow: 1;
+    min-height: calc(100vh - 3.5rem);
+  }
+  footer {
+    position: absolute;
+    width: 100%;
+    flex-grow: 0;
+    flex-shrink: 0;
+    @apply py-5;
+    @apply text-center;
+    @apply bg-slate-300;
+    @apply text-xs;
+  }
+
+  footer a {
+    @apply text-cyan-800;
+    @apply underline;
+  }
+</style>
