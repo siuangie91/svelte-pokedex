c394178ebee8fa2dd2e5b105a37abbe49a0ffa3e
Angie Siu <siuangie91@gmail.com>
2022-08-09 23:36:02 -0400

    style: basic pokemon card

--- src/components/Card/index.svelte
+++ src/components/Card/index.svelte
@@ -8,33 +8,104 @@
   export let types: Type[] = [];
   export let evolution: string[] = [];
   export let description: string = '';
+
+  // const gridTemplateColumns = `repeat(${types.length}, minmax(0, 1fr))`;
 </script>
 
-<h1>{id}. {name}</h1>
+<section>
+  <div>
+    <h1>{id}. {name}</h1>
 
-{#if image}
-  <img src={image} alt={`${name}`} />
-{/if}
+    {#if image}
+      <img src={image} alt={`${name}`} />
+    {/if}
 
-{#if types.length}
-  <h2>Types</h2>
-  <ul>
-    {#each types as { type }}
-      <li>{capitalizeFirstLetter(type.name)}</li>
-    {/each}
-  </ul>
-{/if}
+    {#if types.length}
+      <h2 class="card-h2">Types</h2>
+      <ul class="list-types" style="--num-types: {types.length}">
+        {#each types as { type }}
+          <li>{capitalizeFirstLetter(type.name)}</li>
+        {/each}
+      </ul>
+    {/if}
 
-{#if evolution.length}
-  <h2>Evolution</h2>
-  <ol>
-    {#each evolution as name}
-      <li>{capitalizeFirstLetter(name)}</li>
-    {/each}
-  </ol>
-{/if}
+    {#if evolution.length}
+      <h2 class="card-h2">Evolution</h2>
+      <ol class="list-evolution" style="--num-evolution: {evolution.length}">
+        {#each evolution as name}
+          <li>{capitalizeFirstLetter(name)}</li>
+        {/each}
+      </ol>
+    {/if}
 
-{#if description}
-  <h2>Description</h2>
-  <p>{description}</p>
-{/if}
+    {#if description}
+      <h2 class="card-h2">Description</h2>
+      <p class="description">{description}</p>
+    {/if}
+  </div>
+</section>
+
+<style lang="postcss">
+  section {
+    max-width: 350px;
+    @apply mx-auto;
+    @apply p-4;
+    @apply bg-amber-300;
+    @apply rounded-lg;
+  }
+
+  div {
+    @apply p-4;
+    @apply bg-white;
+  }
+
+  h1 {
+    @apply m-0;
+    @apply text-lg;
+  }
+
+  .card-h2 {
+    @apply text-sm;
+  }
+
+  img {
+    @apply block;
+    @apply w-3/4;
+    @apply mx-auto;
+  }
+
+  .list-types,
+  .list-evolution {
+    @apply grid;
+    @apply gap-4;
+  }
+
+  .list-types {
+    grid-template-columns: repeat(var(--num-types), minmax(0, 1fr));
+  }
+  .list-types li {
+    background-color: gray;
+    @apply rounded-sm;
+    @apply text-center;
+  }
+
+  .list-evolution {
+    grid-template-columns: repeat(var(--num-evolution), minmax(0, 1fr));
+  }
+  .list-evolution li {
+    background-color: gray;
+    @apply rounded-sm;
+    @apply text-center;
+    @apply relative;
+
+    @apply after:content-['→'];
+    @apply after:absolute;
+    @apply after:-right-4;
+
+    @apply last-of-type:after:content-none;
+  }
+
+  .description {
+    @apply text-sm;
+  }
+</style>
