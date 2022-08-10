d82e2f22f10ba50c04655d7242d4dce592453a94
Angie Siu <siuangie91@gmail.com>
2022-08-10 19:56:20 -0400

    feat: wire up store in dropdown and list

--- src/components/Seen/index.svelte
+++ src/components/Seen/index.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import type { PokemonLookup } from 'src/types';
   import Container from 'components/shared/Container.svelte';
   import List from './List.svelte';
-
-  export let entries: PokemonLookup = {};
 </script>
 
 <Container title="Here's what you've seen so far:">
-  <List pokemons={entries} />
+  <List />
 </Container>
