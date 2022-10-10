e4f353bf7322cd89e8f9ca22939074842e19f7be
Angie Siu <siuangie91@gmail.com>
2022-10-10 00:13:10 -0400

    feat: demo server side fetch on root route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,39 @@
+<script context="module" lang="ts">
+  import type { LoadEvent } from '@sveltejs/kit';
+  import type { Generation1 } from 'src/types';
+  import { POKEAPI_GQL_URL } from 'src/network';
+  import first20Gen1Query from 'src/queries/first20Gen1Query';
+
+  export async function load({ fetch }: LoadEvent) {
+    const response = await fetch(POKEAPI_GQL_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        query: first20Gen1Query,
+        variables: {
+          id: 1,
+        },
+        operationName: 'First20Gen1',
+      }),
+    });
+
+    const { data } = await response.json();
+
+    const gen1: Generation1 | null = data?.gen1 ?? null;
+
+    console.log('data', gen1);
+
+    if (!gen1) {
+      throw new Error('failed getting pokedex');
+    }
+
+    return {
+      props: {
+        gen1,
+      },
+    };
+  }
+</script>
+
 <script lang="ts">
   import Selection from 'components/Selection/index.svelte';
   import Seen from 'components/Seen/index.svelte';
