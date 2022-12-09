e6c0b6c4f1d808ea046f0a8ce60febb9085a437e
Angie Siu <siuangie91@gmail.com>
2022-12-08 22:32:26 -0500

    chore: use single quote

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -53,6 +53,6 @@ module.exports = {
       'warn',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
-    indent: ["error", 2],
+    indent: ['error', 2],
   },
 };
