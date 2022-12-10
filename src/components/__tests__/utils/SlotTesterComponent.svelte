076b9cb184bbc00f6d9cab9e1bf21364ca30ddf5
Angie Siu <siuangie91@gmail.com>
2022-12-10 18:18:24 -0500

    test: Container slot

---
+++ src/components/__tests__/utils/SlotTesterComponent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  /**
+   * This is a pass-through component for testing purposes,
+   * as there is no way to test <slot />.
+   * It renders a component dynamically,
+   * whose <slot /> will be the element [data-testid="slot"].
+   * @see https://svelte.dev/docs#template-syntax-svelte-component
+   * @see https://sveltesociety.dev/recipes/testing-and-debugging/unit-testing-svelte-component
+   */
+
+  /**
+   * Generic type for components
+   * @see https://www.viget.com/articles/typing-components-in-svelte/
+   */
+  import type { ComponentType } from 'svelte';
+
+  export let Component: ComponentType;
+</script>
+
+<svelte:component this={Component}>
+  <div data-testid="slot">Test Slot Content</div>
+</svelte:component>
--- src/components/shared/__tests__/Container.test.ts
+++ src/components/shared/__tests__/Container.test.ts
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/svelte';
+import SlotTesterComponent from 'src/components/__tests__/utils/SlotTesterComponent.svelte';
 import Container from '../Container.svelte';
 
 describe('Container', () => {
@@ -13,5 +14,15 @@ describe('Container', () => {
     const titleElem = screen.getByText(title);
 
     expect(titleElem).toBeInTheDocument();
+  });
+
+  it('renders the provided slot component', () => {
+    render(SlotTesterComponent, {
+      Component: Container
+    });
+
+    const slotElem = screen.getByTestId('slot');
+
+    expect(slotElem).toBeInTheDocument();
   });
 });
